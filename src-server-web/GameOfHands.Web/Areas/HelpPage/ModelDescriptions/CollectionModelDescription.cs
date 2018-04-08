061d0f88ea144ee586986613279e96e631b84338
Soham Joshi <itsmylifesoham@gmail.com>
2018-04-08 15:35:09 -0400

    adding asp.net mvc and  web api web server project

---
+++ src-server-web/GameOfHands.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace GameOfHands.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ src-server-web/GameOfHands.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GameOfHands.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
