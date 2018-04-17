0783005c31940e48f995ec404e5687f6dc441cbb
Soham Joshi <itsmylifesoham@gmail.com>
2018-04-17 07:46:58 -0400

    updating sfs config

--- src-client/merges/android/app/globals.js
+++ src-client/merges/android/app/globals.js
@@ -4,7 +4,7 @@ define(function (require) {
         appName: 'goh',
         websiteEndPoint: 'https://192.168.0.102/',
         sfsConfig: {
-            host: '10.209.16.176',
+            host: '192.168.0.102',
             port: 8080,
             zone: 'BasicExamples',
             useSSL: false,
